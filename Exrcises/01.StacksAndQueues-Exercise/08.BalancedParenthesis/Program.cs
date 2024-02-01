3bb6db703d1d6e9691345c8b32678f68e5784ebd
hyuliasadula <huliya1401@gmail.com>
2024-02-01 13:26:35 +0200

    7-Unfinished   8-Finished

---
+++ Exrcises/01.StacksAndQueues-Exercise/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancedParentheses
+{
+    /*
+     {[()]} - This is a balanced parenthesis.
+     {[(])} - This is not a balanced parenthesis.
+
+  	 For each test case, print on a new line "YES", if the parentheses are balanced. 
+     Otherwise, print "NO". Do not print the quotes.
+
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sequence = Console.ReadLine();
+
+            bool isBalanced = IsBalancedParentheses(sequence);
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        static bool IsBalancedParentheses(string sequence)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char parenthesis in sequence)
+            {
+                if (parenthesis == '(' || parenthesis == '{' || parenthesis == '[')
+                {
+                    stack.Push(parenthesis);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false; // Unmatched closing parenthesis
+                    }
+
+                    char openParenthesis = stack.Pop();
+
+                    /*
+                        If parenthesis is ')' but openParenthesis is not '(', or
+                        If parenthesis is '}' but openParenthesis is not '{', or
+                        If parenthesis is ']' but openParenthesis is not '['
+                     */
+                    if ((parenthesis == ')' && openParenthesis != '(') ||
+                        (parenthesis == '}' && openParenthesis != '{') ||
+                        (parenthesis == ']' && openParenthesis != '['))
+                    {
+                        return false; // Mismatched closing parenthesis
+                    }
+                }
+            }
+
+            return stack.Count == 0; // The sequence is balanced if the stack is empty
+        }
+    }
+}

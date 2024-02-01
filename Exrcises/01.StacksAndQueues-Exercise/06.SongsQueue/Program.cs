3f152dfb4d4684d46430fbbb8ce3ac08d7a48d93
hyuliasadula <huliya1401@gmail.com>
2024-02-01 13:04:28 +0200

    Added a comment with the task

--- Exrcises/01.StacksAndQueues-Exercise/06.SongsQueue/Program.cs
+++ Exrcises/01.StacksAndQueues-Exercise/06.SongsQueue/Program.cs
@@ -4,6 +4,30 @@ using System.Collections.Generic;
 
 namespace _06.SongsQueue
 {
+    /*
+     * 
+     * 
+     * 
+     * 
+     * You will be given a sequence of songs, separated by a comma and a single space. After that, you will be given commands until there are no songs enqueued. When there are no more songs in the queue print "No more songs!" and stop the program.
+        The possible commands are:
+        •	"Play" - plays a song (removes it from the queue)
+        •	"Add {song}" - adds the song to the queue, if it isn't contained already, otherwise print "{song} is already contained!"
+        •	"Show" - prints all songs in the queue, separated by a comma and a white space (start from the first song in the queue to the last)
+
+     Input                                  Output
+    All Over Again, Watch Me                Watch Me is already contained!
+    Play                                    Watch Me, Love Me Harder, Promises
+    Add Watch Me                            No more songs!
+    Add Love Me Harder
+    Add Promises
+    Show
+    Play
+    Play
+    Play
+    Play
+
+     */
     internal class Program
     {
         static void Main(string[] args)

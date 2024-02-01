f12016df809a3060e5eae00e6ec653ed6fb1434d
hyuliasadula <huliya1401@gmail.com>
2024-02-01 12:28:47 +0200

    Finished ex5

--- Exrcises/01.StacksAndQueues-Exercise/05.FashionBoutique/Program.cs
+++ Exrcises/01.StacksAndQueues-Exercise/05.FashionBoutique/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 
 namespace _05.FashionBoutique
 {
+    /*
+     Input
+    •	On the first line, you will be given a sequence of integers, representing the clothes in the box, separated by a single space.
+    •	On the second line, you will be given an integer, representing the capacity of a rack.
+
+
+    Output
+    •	Print the number of racks, needed to hang all of the clothes from the box
+
+     */
     internal class Program
     {
         static void Main(string[] args)
@@ -12,6 +22,30 @@ namespace _05.FashionBoutique
             Stack<int> itemsInBoxes = new Stack<int>(clothesInABox);
             int capacityOfARack = int.Parse(Console.ReadLine());
 
+            int racks = 0;
+            int currentRackSum = 0;
+
+            while (itemsInBoxes.Count > 0)
+            {
+                int currentClothing = itemsInBoxes.Pop();
+
+                if (currentRackSum + currentClothing <= capacityOfARack)
+                {
+                    currentRackSum += currentClothing;
+                }
+                else
+                {
+                    racks++;
+                    currentRackSum = currentClothing;
+                }
+            }
+
+            if (currentRackSum > 0)
+            {
+                racks++;
+            }
+
+            Console.WriteLine(racks);
         }
     }
 }

227d5164d756382c37a67100e5fe60fc71ceb83c
hyuliasadula <huliya1401@gmail.com>
2024-02-02 10:59:59 +0200

    Finished ex 3

--- Lections/02.MultidimensionalArrays/02.MultidimensionalArrays/03.PrimaryDiagonal/Program.cs
+++ Lections/02.MultidimensionalArrays/02.MultidimensionalArrays/03.PrimaryDiagonal/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03.PrimaryDiagonal
 {
     internal class Program
     {
+        /* Sum of elements from the matrix’s primary diagonal
+         * 
+         1 2 3   => 1 + 5 + 9 = 15
+         4 5 6
+         7 8 9
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sizeOfSquareMatrix = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[sizeOfSquareMatrix, sizeOfSquareMatrix];
+
+            for (int row = 0; row < sizeOfSquareMatrix; row++)
+            {
+                int[] data = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < sizeOfSquareMatrix; col++)
+                {
+                    matrix[row, col] = data[col];
+                }
+            }
+
+            int sum = CalculatePrimaryDiagonalSum(matrix, sizeOfSquareMatrix);
+            Console.WriteLine(sum);
+        }
+
+        static int CalculatePrimaryDiagonalSum(int[,] matrix, int sizeOfSquareMatrix)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < sizeOfSquareMatrix; i++)
+            {
+                sum += matrix[i, i];
+            }
+
+            return sum;
         }
     }
 }

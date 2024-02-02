05a7fe9de0c573d41f318e2c2eee9b48b82040cd
hyuliasadula <huliya1401@gmail.com>
2024-02-02 10:19:45 +0200

    Finished Ex 7 and 9

--- Exrcises/01.StacksAndQueues-Exercise/07.TruckTour/Program.cs
+++ Exrcises/01.StacksAndQueues-Exercise/07.TruckTour/Program.cs
@@ -6,7 +6,44 @@ namespace _07.TruckTour
     {
         static void Main(string[] args)
         {
-            int petrolPumps = int.Parse(Console.ReadLine());
+            int numPetrolPumps = int.Parse(Console.ReadLine());
+            int[] petrolAmounts = new int[numPetrolPumps];
+            int[] distances = new int[numPetrolPumps];
+
+            for (int i = 0; i < numPetrolPumps; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                petrolAmounts[i] = int.Parse(input[0]); // Amount of petrol
+                distances[i] = int.Parse(input[1]); // Distance to next petrol pump
+            }
+
+            int startPetrolPump = FindStartPetrolPump(numPetrolPumps, petrolAmounts, distances);
+
+            Console.WriteLine(startPetrolPump);
+        }
+
+        static int FindStartPetrolPump(int numPetrolPumps, int[] petrolAmounts, int[] distances)
+        {
+            int totalPetrol = 0;
+            int currentPetrol = 0;
+            int startPetrolPumpIndex = 0;
+
+            for (int i = 0; i < numPetrolPumps; i++)
+            {
+                totalPetrol += petrolAmounts[i] - distances[i];
+
+                // If the current petrol is negative, reset starting point and current petrol
+                if (currentPetrol < 0)
+                {
+                    startPetrolPumpIndex = i;
+                    currentPetrol = 0;
+                }
+
+                currentPetrol += petrolAmounts[i] - distances[i];
+            }
+
+            return totalPetrol >= 0 ? startPetrolPumpIndex : -1;
         }
     }
 }
+
---
+++ Exrcises/01.StacksAndQueues-Exercise/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.SimpleTextEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfOperations = int.Parse(Console.ReadLine());
+            StringBuilder text = new StringBuilder();
+            Stack<string> operationStack = new Stack<string>();
+            operationStack.Push("");
+
+            for (int i = 0; i < numOfOperations; i++)
+            {
+                string[] commands = Console.ReadLine().Split(' ');
+
+                int operationType = int.Parse(commands[0]);
+
+                switch (operationType)
+                {
+                    case 1:
+                        // Append
+                        string appendedText = commands[1];
+                        text.Append(appendedText);
+                        operationStack.Push(text.ToString());
+                        break;
+
+                    case 2:
+                        // Delete
+                        int charsToDelete = int.Parse(commands[1]);
+                        text.Length -= charsToDelete;
+                        operationStack.Push(text.ToString());
+                        break;
+
+                    case 3:
+                        // Print
+                        int indexToPrint = int.Parse(commands[1]) - 1;
+                        if (indexToPrint >= 0 && indexToPrint < text.Length)
+                        {
+                            Console.WriteLine(text[indexToPrint]);
+                        }
+                        break;
+
+                    case 4:
+                        // Undo
+                        if (operationStack.Count > 1)
+                        {
+                            operationStack.Pop(); // Discard the current state
+                            text = new StringBuilder(operationStack.Peek()); // Revert to the previous state
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
